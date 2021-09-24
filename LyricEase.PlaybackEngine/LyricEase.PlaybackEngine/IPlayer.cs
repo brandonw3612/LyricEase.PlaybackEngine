d2ad4df7947213e988c47b01c13dc82320f29d88
Brandon Wong <brandonw3612@outlook.com>
2021-09-24 16:02:42 +0800

    Create models and IPlayer interface.

---
+++ LyricEase.PlaybackEngine/LyricEase.PlaybackEngine/IPlayer.cs
@@ -0,0 +1,49 @@
+﻿using LyricEase.PlaybackEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LyricEase.PlaybackEngine
+{
+    interface IPlayer
+    {
+        double Volume { get; set; }
+        bool IsDirectSwitchEnabled { get; set; }
+        SoundQuality SoundQuality { get; set; }
+        PlaybackMode PlaybackMode { get; set; }
+
+        bool? IsPlaying { get; }
+        object PlaybackSource { get; }
+        ITrack CurrentItem { get; }
+
+        bool IsPreviousItemAvailable { get; }
+        bool IsNextItemAvailable { get; }
+
+        List<ITrack> OriginalPlaybackList { get; }
+        Stack<ITrack> UpNextPlaybackStack { get; }
+        List<int> PlaybackOrder { get; }
+        List<ITrack> NextPlayingQueue { get; }
+        
+        void PlayList(IEnumerable<ITrack> Items, ITrack StartingItem, object PlaybackSource);
+        void AddToUpNext(IEnumerable<ITrack> Items);
+        void Previous();
+        void Next();
+        void Play();
+        void Pause();
+        void PlayPause();
+        void Stop();
+        void Seek(TimeSpan TargetPosition);
+        void SkipToIndex(bool IsMainPlaybackQueue, int TargetItemIndex);
+        void RemoveItem(bool IsMainPlaybackQueue, int TargetItemIndex);
+        
+        event EventHandler<PlaybackStatusChangedEventArgs> PlaybackStatusChanged;
+        event EventHandler<EventArgs> PlaybackModeChanged;
+        event EventHandler<PlaybackPositionChangedEventArgs> PlaybackPositionChanged;
+        event EventHandler<CurrentlyPlayingItemChangedEventArgs> CurrentlyPlayingItemChanged;
+        event EventHandler PlaybackQueueUpdated;
+        event EventHandler PlaybackEnded;
+        event EventHandler<PlaybackErrorEventArgs> PlaybackError;
+    }
+}
---
+++ LyricEase.PlaybackEngine/LyricEase.PlaybackEngine/Models.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LyricEase.PlaybackEngine.Models
+{
+    public enum SoundQuality { Normal, High, VeryHigh, Lossless }
+
+    public enum PlaybackMode { RepeatAll, RepeatOne, Shuffle }
+
+    public interface ITrack
+    {
+
+    }
+
+    public sealed class PlaybackStatusChangedEventArgs
+    {
+        public bool IsPlaying { get; set; }
+        public bool PreviousAvailable { get; set; }
+        public bool NextAvailable { get; set; }
+    }
+
+    public sealed class PlaybackPositionChangedEventArgs
+    {
+        public TimeSpan Current { get; set; }
+        public TimeSpan Total { get; set; }
+    }
+
+    public sealed class CurrentlyPlayingItemChangedEventArgs
+    {
+        public ITrack CurrentTrack { get; set; }
+    }
+
+    public sealed class PlaybackErrorEventArgs
+    {
+        public string Message { get; set; }
+    }
+}
