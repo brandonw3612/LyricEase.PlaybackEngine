64e4c6d54d1fc523082a7232cb11bdb483f4e617
merlin-Y <1411194058@qq.com>
2021-09-25 14:47:26 +0800

    Create ApplicationSettingsExtension

---
+++ LyricEase.PlaybackEngine/ApplicationSettingsExtension.cs
@@ -0,0 +1,71 @@
+﻿using LyricEase.PlaybackEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace LyricEase.PlaybackEngine
+{
+    internal static class ApplicationSettingsExtension
+    {
+        private static ApplicationDataContainer RootContainer => ApplicationData.Current.LocalSettings.CreateContainer("DoxPlayerSettings", ApplicationDataCreateDisposition.Always);
+
+        #region Settings Items
+
+        public static Models.PlaybackMode PlaybackMode
+        {
+            get => (Models.PlaybackMode)PlaybackModeProperty.GetValue();
+            set => PlaybackModeProperty.SetValue((int)value);
+        }
+        private static readonly SettingProperty<int> PlaybackModeProperty = new(nameof(PlaybackMode), 0);
+
+        public static double Volume
+        {
+            get => VolumeProperty.GetValue();
+            set => VolumeProperty.SetValue(value);
+        }
+        private static readonly SettingProperty<double> VolumeProperty = new(nameof(Volume), 1.0d);
+        
+        public static SoundQuality SoundQuality
+        {
+            get => (SoundQuality)SoundQualityProperty.GetValue();
+            set => SoundQualityProperty.SetValue((int)value);
+        }
+        private static readonly SettingProperty<int> SoundQualityProperty = new(nameof(SoundQuality), 2);
+        
+        public static bool IsDirectSwitchEnabled
+        {
+            get => IsDirectSwitchEnabledProperty.GetValue();
+            set => IsDirectSwitchEnabledProperty.SetValue(value);
+        }
+        private static readonly SettingProperty<bool> IsDirectSwitchEnabledProperty = new(nameof(IsDirectSwitchEnabled), false);
+
+        #endregion
+
+        private sealed class SettingProperty<T>
+        {
+            private readonly string propertyName;
+            private readonly T defaultValue;
+
+            public SettingProperty(string PropertyName, T DefaultValue)
+            {
+                propertyName = PropertyName;
+                defaultValue = DefaultValue;
+            }
+
+            public T GetValue()
+            {
+                if (!RootContainer.Values.ContainsKey(propertyName)) return defaultValue;
+                return (T)RootContainer.Values[propertyName];
+            }
+
+            public void SetValue(T value) => RootContainer.Values[propertyName] = value;
+
+            public void RemoveValue() => RootContainer.Values.Remove(propertyName);
+
+            public T DefaultValue => defaultValue;
+        }
+    }
+}
